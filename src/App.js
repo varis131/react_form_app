78ff9edd669dcdcd5ad25431ca8e3da5ea6e274d
varis131 <movaris85@gmail.com>
2026-01-19 16:21:10 +0530

    intializes the formapp using react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      FormApp
-    </div>
-  );
+  return <div className="App">FormApp</div>;
 }
 
 export default App;
